0e458e532ce9a9c837faaca6dca1182e46bdcea3
bhanu-prasad210 <bhanuthigulla21@gmail.com>
2025-04-07 19:52:43 -0400

    Add files via upload

---
+++ src/main/java/com/hexa/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.hexa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "✅ Application is working successfully from EKS!";
+    }
+}
